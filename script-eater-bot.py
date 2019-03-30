42fbea27aa15f66a4d67de3121ea4b59d1c3783b
the-ivo-robotnic <theivorobotnic@gmail.com>
2019-03-30 03:42:27 -0700

    anonymous update

--- script-eater-bot.py
+++ script-eater-bot.py
@@ -38,16 +38,20 @@ def load_config(filename):
     config = json.loads(config_file.read())
     config_file.close()
 
+    config['response-timeout'] = float(config['response-timeout'])
+
     return config
 
 def save_config(filename):
     global config
 
+    dumpable_data = json.dumps(config, skipkeys=False, ensure_ascii=True, check_circular=True, allow_nan=True, cls=None, indent=4, separators=None, default=None, sort_keys=True)
+
     if(config['debug']):
-        say(2, json.dumps(config))
+        say(2, dumpable_data)
 
     config_file = open(filename, mode='w', buffering=-1, encoding=None, errors=None, newline='\n', closefd=True)
-    config_file.write(json.dumps(config))
+    config_file.write(dumpable_data)
     config_file.close()
 
 def update_default_bot_status():
@@ -77,6 +81,14 @@ def message_argument(message):
 def is_command(message):
     return (message_prefix(message) == config['prefix'])
 
+def is_admin(user_id):
+    global admins
+
+    for i in admins:
+        if(user_id == i):
+            return True
+    return False
+
 def get_command_map(message):
     index = 0
 
@@ -93,6 +105,15 @@ def load_servers():
     say(0, 'Script Eater has joined a total of ' + str(len(client.servers)) + ' servers!')
     for i in client.servers:
         print('\t\"' + i.name + '\" in ' + str(i.region) + ' with ' + str(len(i.members)) + ' people')
+
+def load_admins():
+    global client
+    global config
+    global admins
+
+    for i in config['admins']:
+        admins.append(client.get_user_info(i))
+        say(1, "Adding " + str(client.get_user_info(i).name))
 
 def load_emojis():
     global client
@@ -118,22 +139,20 @@ async def set_status(status_message, new_status):
 
 async def resolve_confirmation(message, question):
     global client
-    global response_timeout
-    global confirm_emoji
-    global deny_emoji
+    global config
 
     response = await client.send_message(message.channel, content=(question))
-    await client.add_reaction(response, confirm_emoji)
-    await client.add_reaction(response, deny_emoji)
+    await client.add_reaction(response, config['confirm-emoji'])
+    await client.add_reaction(response, config['deny-emoji'])
 
-    resolution = await client.wait_for_reaction(emoji=[confirm_emoji, deny_emoji], message=response, user=message.author, timeout=response_timeout)
+    resolution = await client.wait_for_reaction(emoji=[config['confirm-emoji'], config['deny-emoji']], message=response, user=message.author, timeout=config['response-timeout'])
     await client.delete_message(response)
 
     if(resolution == None):
         return None
-    elif(resolution[0].emoji == confirm_emoji):
+    elif(resolution[0].emoji == config['confirm-emoji']):
         return True
-    elif(resolution[0].emoji == deny_emoji):
+    elif(resolution[0].emoji == config['deny-emoji']):
         return False
     else:
         say(3, "Reaction confirmation could not resolve!")
@@ -160,31 +179,49 @@ async def set_new_prefix(message):
     global config
     global client
 
-    try:
-        argument = message_argument(message.content)
-    except:
-        say(3, "No argument was given for the prefix command!\n\tTerminating the thread early!")
-        await client.delete_message(message)
-        await client.send_message(message.channel, content='**Incorrect usage! Please specify a new prefix!**')
-        return
+    # Check if user has permission to do this
+    if(is_admin(message.author.id)):
+        try:
+            argument = message_argument(message.content)
+        except:
+            say(3, "No argument was given for the prefix command!\n\tTerminating the thread early!")
+            await client.delete_message(message)
+            await client.send_message(message.channel, content='**Incorrect usage! Please specify a new prefix!**')
+            return
 
-    resolution = await resolve_confirmation(message, "**Set new prefix to \"" + argument + "\"?**")
+        resolution = await resolve_confirmation(message, "**Set new prefix to \"" + argument + "\"?**")
 
-    if(resolution == None):
-        say(3, "No response was given!\n\tTerminating the thread early!")
-        await client.send_message(message.channel, content='**No response was given! Canceling request...**')
-    elif(resolution == True):
-        config['prefix'] = argument
-        update_default_bot_status()
-        await client.send_message(message.channel, content='**Changed prefix to: \"' + argument + "\"!**")
-    elif(resolution == False):
-        await client.send_message(message.channel, content='**Keeping prefix as ' + config['prefix'] + '**')
+        if(resolution == None):
+            say(3, "No response was given!\n\tTerminating the thread early!")
+            await client.send_message(message.channel, content='**No response was given! Canceling request...**')
+        elif(resolution == True):
+            config['prefix'] = argument
+            update_default_bot_status()
+            await client.send_message(message.channel, content='**Changed prefix to: \"' + argument + "\"!**")
+        elif(resolution == False):
+            await client.send_message(message.channel, content='**Keeping prefix as ' + config['prefix'] + '**')
+    else:
+        await client.send_message(message.channel, content='**Must be an admin to do this!**')
 
     await client.delete_message(message)
 
 async def format_script(message):
     await asyncio.sleep(10)
     return
+
+async def revert_config(message):
+    global client
+    global config
+    global config_path
+
+    if(is_admin(message.author.id)):
+        config = load_config(config_path)
+        update_default_bot_status()
+        await client.send_message(message.channel, content='**Reverted bot config to settings on-file!**')
+    else:
+        await client.send_message(message.channel, content='**Must be an admin to do this!**')
+
+    await client.delete_message(message)
 
 async def bad_command(message):
     global client
@@ -204,10 +241,8 @@ bot_token = os.getenv('SCRIPT_EATER_TOKEN')
 #
 # Stuff to use in runtime
 #
-response_timeout = 10.0 # Seconds
 active_bot_status = ''
-confirm_emoji = '✅'
-deny_emoji = '⛔'
+admins = []
 emojis = []
 ftf_website = 'https://foxtrotfanatics.info'
 ftf_logo_url = 'https://media.foxtrotfanatics.info/i/ftf_logo.png'
@@ -225,6 +260,7 @@ async def on_ready():
 
     say(0, 'Client logged in as the user ' + str(client.user) + '\n\tID: ' + str(client.user.id) + '\n\tBOT: ' + str(client.user.bot) + '\n\tDisplay Name: ' + str(client.user.display_name))
     load_servers()
+    load_admins()
     load_emojis()
     await set_status(active_bot_status, discord.Status.idle)
 
@@ -240,15 +276,24 @@ async def on_message(message):
         # Get the action map of the command suffix
         map = get_command_map(message.content)
 
-        if(map == 0): # help
-            await set_status('Prepairing help...', discord.Status.dnd)
-            await usage(message)
-        elif(map == 1): # prefix
-            await set_status('Waiting for response...', discord.Status.dnd)
-            await set_new_prefix(message)
+        if(map == 0): # addAdmin
+            await set_status('Adding a new admin...', discord.Status.dnd)
+        elif(map == 1): # admins
+            await set_status('Getting admins...', discord.Status.dnd)
         elif(map == 2): # format
             await set_status('Formatting script...', discord.Status.dnd)
             await format_script(message)
+        elif(map == 3): # help
+            await set_status('Prepairing help...', discord.Status.dnd)
+            await usage(message)
+        elif(map == 4): # prefix
+            await set_status('Waiting for response...', discord.Status.dnd)
+            await set_new_prefix(message)
+        elif(map == 5): # removeAdmin
+            await set_status('Removing old admin...', discord.Status.dnd)
+        elif(map == 6): # revertConfig
+            await set_status('Overwiting config...', discord.Status.dnd)
+            await revert_config(message)
         else:
             await bad_command(message)
 
